ba07351b0fd9f73d34455db87e31f25bd171ae8a
Paolo Vargas <paolo9517@gmail.com>
2021-11-19 17:28:25 -0300

    Home page Views Setup and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'for_providers', to: 'pages#for_providers'
+  get 'for_shelters', to: 'pages#for_shelters'
+  get 'about_us', to: 'pages#about_us'
+  get 'contact_us', to: 'pages#contact_us'
+
+
 end
