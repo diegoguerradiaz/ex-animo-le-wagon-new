036ed735bbc3db507f19c17abc4bad13ad14ec2d
Diego Guerra <diegoguerradiaz@gmail.com>
2021-11-20 15:31:12 -0300

    created new and create methods for food offers

--- app/controllers/food_offers_controller.rb
+++ app/controllers/food_offers_controller.rb
@@ -8,9 +8,17 @@ class FoodOffersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @food_offer = FoodOffer.new(params[:food_offer])
+    @food_offer = FoodOffer.new(food_offer_params)
+    @food_offer.user = current_user
+    # @user = User.find(params[user_id])
+    # @food_offer.user_id = @user
     @food_offer.save
+    # redirect_to food_offer_path(@food_offer)
   end
 
+  private
+
+  def food_offer_params
+    params.require(:food_offer).permit(:category, :product_name, :unit)
+  end
 end
