1fe51037a0451dbaa8caa33a86c68ec5be48dc95
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2021-11-30 21:36:56 -0300

    datepicker & no results message

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,11 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initFlatpickr } from "../plugins/flatpicker";
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initFlatpickr();
 });
